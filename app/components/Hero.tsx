0ded5a06b650c2ad3d4f89da4e9a5744a5350dbf
jonatashborba-lab <jonatas.hborba@gmail.com>
2026-02-27 01:57:12 +0000

    fix hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero({
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage: url(${image}),
+          backgroundImage: `url(${image})`,
         }}
       />
 
